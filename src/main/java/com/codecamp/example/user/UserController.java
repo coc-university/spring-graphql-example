0146077be884ebf7edde2dc7ad82f9c11965d542
RobinSchneiderCOC <robin.schneider@codecamp-n.com>
2025-05-19 16:00:59 +0200

    add input argument

--- src/main/java/com/codecamp/example/user/UserController.java
+++ src/main/java/com/codecamp/example/user/UserController.java
@@ -1,11 +1,14 @@
 package com.codecamp.example.user;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
 @Controller
+@Slf4j
 public class UserController {
 
     @QueryMapping
@@ -14,8 +17,9 @@ public class UserController {
     }
 
     @QueryMapping
-    public User user() {
-        return new User("Robin", "test");
+    public User user(@Argument String name) {
+        log.info("User Name: {}", name);
+        return new User(name, "test");
     }
 
     @QueryMapping
